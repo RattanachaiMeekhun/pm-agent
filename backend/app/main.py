686ad4b07532373d7e4ca7684ebb2471e1685d5b
RattanachaiMeekhun <75651308+RattanachaiMeekhun@users.noreply.github.com>
2026-03-03 21:53:13 +0700

    add allow cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers import project, sow, qa, auth
-from app.db.session import engine   
+from app.db.session import engine
 from app.db import models
 from pwdlib import PasswordHash
 
@@ -12,6 +12,7 @@ password_hash = PasswordHash.recommended()
 
 # Configure CORS
 origins = [
+    "https://pm-agent-lyart.vercel.app",
     "http://localhost:3000",  # React app
     "http://localhost:8000",
 ]
@@ -32,6 +33,7 @@ app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(project.router, prefix="/api/v1/project", tags=["Project"])
 app.include_router(sow.router, prefix="/api/v1", tags=["SOW"])
 app.include_router(qa.router, prefix="/api/v1/qa", tags=["QA Agent"])
+
 
 @app.get("/")
 async def root():
